4955fab8f75ccbe86b8d15e601ca0afcdd1c5d57
Yaroslav <slavik2607.lviv@gmail.com>
2023-05-25 13:30:54 +0300

    All tasks have been finished

--- homework№3/script.js
+++ homework№3/script.js
@@ -63,6 +63,9 @@
 //   let secondArr = arr.filter((element) => typeof element != "number");
 //   console.log(firstArr);
 //   console.log(secondArr);
+//   let finalArray = [];
+//   finalArray.push(firstArr, secondArr);
+//   console.log(finalArray);
 // }
 // funcName([
 //   5,
@@ -78,4 +81,73 @@
 // ]);
 //---------------------------------------------------------
 
-// Завдання №6
+// Завдання №7 (Для закріплення)
+// function findUnique(arr) {
+//   let map = new Map();
+//   let array = arr.map((value, index, ar) => {
+//     if (map.has(value)) {
+//       console.log(`Масив не унікальний`);
+//       return false;
+//     } else {
+//       map.set(value, true);
+//       console.log(`Масив унікальний`);
+//       return true;
+//     }
+//   });
+//   console.log(array);
+// }
+//---------------------------------------------------------
+
+// Завдання №8
+// let ask = +prompt("Write an hour");
+// let night = [23, 0, 1, 2, 3, 4, 5];
+// let morning = [6, 7, 8, 9, 10, 11];
+// let day = [12, 13, 14, 15, 16, 17];
+// let evening = [18, 19, 20, 21, 22];
+// if (night.includes(ask)) {
+//   alert("Доброї ночі");
+// } else if (morning.includes(ask)) {
+//   alert("Доброго ранку");
+// } else if (day.includes(ask)) {
+//   alert("Доброго дня");
+// } else if (evening.includes(ask)) {
+//   alert("Доброго вечора");
+// } else {
+//   alert("Введіть вірне число");
+// }
+// switch (ask) {
+//   case 23:
+//   case 0:
+//   case 1:
+//   case 2:
+//   case 3:
+//   case 4:
+//   case 5:
+//     alert("Доброї ночі");
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//   case 9:
+//   case 10:
+//   case 11:
+//     alert("Доброго ранку");
+//     break;
+//   case 12:
+//   case 13:
+//   case 14:
+//   case 15:
+//   case 16:
+//   case 17:
+//     alert("Доброго дня");
+//     break;
+//   case 18:
+//   case 19:
+//   case 20:
+//   case 21:
+//   case 22:
+//     alert("Доброго вечора");
+//     break;
+//   default:
+//     alert("Введіть число від 0 до 23");
+// }
