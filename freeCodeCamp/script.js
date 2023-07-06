d0d81b32d343eb746481c226dc3d1857a6b9a199
Yaroslav <slavik2607.lviv@gmail.com>
2023-07-06 14:32:06 +0300

    some practice

--- freeCodeCamp/script.js
+++ freeCodeCamp/script.js
@@ -1,20 +1,46 @@
 //---------------------------------------------------------
-function titleCase(str) {
-  let firstL = "";
-  let lowerWord = "";
-  let result = "";
-  let string = str.split(" ");
-  for (let i of string) {
-    firstL = i.slice(0, 1).toUpperCase();
-    lowerWord = i.slice(1).toLowerCase();
-    result += firstL.concat(lowerWord) + " ";
-  }
-  return result.trim();
-}
+// function titleCase(str) {
+//   let firstL = "";
+//   let lowerWord = "";
+//   let result = "";
+//   let string = str.split(" ");
+//   for (let i of string) {
+//     firstL = i.slice(0, 1).toUpperCase();
+//     lowerWord = i.slice(1).toLowerCase();
+//     result += firstL.concat(lowerWord) + " ";
+//   }
+//   return result.trim();
+// }
 
-titleCase("I'm a little tea pot");
-titleCase("sHoRt AnD sToUt");
-titleCase("HERE IS MY HANDLE HERE IS MY SPOUT");
+// titleCase("I'm a little tea pot");
+// titleCase("sHoRt AnD sToUt");
+// titleCase("HERE IS MY HANDLE HERE IS MY SPOUT");
 //---------------------------------------------------------
 
-// Завдання №4
+// function mutation(arr) {
+//   let firstWord = arr[0].toLowerCase();
+//   let secondWord = arr[1].toLowerCase();
+//   for (let i = 0; i < secondWord.length; i++) {
+//     if (firstWord.indexOf(secondWord[i]) === -1) {
+//       console.log(false);
+//       return;
+//     }
+//   }
+//   console.log(true);
+// }
+
+// mutation(["hello", "hey"]);
+// mutation(["hello", "neo"]);
+// mutation(["floor", "for"]);
+//---------------------------------------------------------
+// function chunkArrayInGroups(arr, size) {
+//   let result = [];
+//   for (let i = 0; i < arr.length; i = i + size) {
+//     let array = arr.slice(i, i + size);
+//     result.push(array);
+//   }
+//   return result;
+// }
+
+// chunkArrayInGroups(["a", "b", "c", "d"], 2);
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);
